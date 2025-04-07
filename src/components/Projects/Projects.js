613cf2f90eeef1437e0aa87f47c8f360f4191fe8
vatsal <22ce020@charusat.edu.in>
2025-04-07 17:10:09 +0530

    Change repo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,7 +23,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-             // imgPath={chatify}
+            
               isBlog={false}
               title="Resume Builder"
               description="In this open-source project, users are not required to log in. They can simplyenter their information such as phone number,language proficiency,projectdetails,education etc., into a form. Once the information is added,users can choose from a selection of more than five templates. After selecting atemplate, users can download it as a PDF file."
@@ -33,18 +33,18 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-             // imgPath={bitsOfCode}
+         
               isBlog={false}
               title="Skill hub"
               description="Developed a web site featuring dual interfaces: an admin dashboard and a user interface. Implemented user authentication functionalities including registration, login, and password recovery.Users can securely input and manage their information through a user-friendly form upon logging in.On the admin side, integrated features enabling administrators to efficiently manage user data, including viewing all user profiles and applying filters based on skills and areas of interest.Empowered users to engage in interactive communication by creating posts and commenting on other user contributions.
 "
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              ghLink="https://github.com/vatsal9972/skill-hub"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-             // imgPath={editor}
+        
               isBlog={false}
               title="Automatic Number Plate Detection on AWS "
               description="Designed and implemented an AWS-based architecture for processing unstructured video    data. Utilized 
